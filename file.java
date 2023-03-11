36b75778fb4a0b4c43324ec1f16939e6c098ff8e
sai <abc@xyz.com>
2023-03-11 02:12:28 +0000

    Modified jav file

--- file.java
+++ file.java
@@ -1,4 +1,5 @@
-public class mapper(jscbkjc)
-  jbe
-  cknekcnl
-  
+public class file{
+       public static void main(String args[]){
+       System.out.println("Welcome to Java!");
+       }
+}       
